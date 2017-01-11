690053b8dd3ae0549f7521e51b1e88957437b841
Michal Skop <michal.skop@kohovolit.eu>
2017-01-11 04:56:56 +0100

    settings example

---
+++ ihned.cz/example-settings.py
@@ -0,0 +1,20 @@
+import sys
+sys.path.append('../')
+path = "ihned.cz/"   # path from the project root
+
+datapackage_path = "http://localhost/michal/project/media-scrapers/ihned.cz/data/"
+
+categories = ['byznys', 'domaci', 'zahranicni', 'nazory', 'life', 'tech', 'art', 'auto', 'archiv']
+
+date_distance = 1
+
+api_code = "XXX"
+api_code_ihned = "XXX"
+
+forum = "ihnedcz"
+
+# settings for MakaBot
+git_dir = "/home/user/project/media-scrapers/"
+ssh_file = "/home/user/.ssh/ExampleBot"
+bot_name = "ExampleBor"
+bot_email = "examplebot@example.com"
